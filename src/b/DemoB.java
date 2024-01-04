5410a410b8eb389e10d2c36985c26c662c6a502a
Samuel <Samuel@192.168.0.21>
2024-01-03 18:03:14 -0800

    Initial Commit

---
+++ src/b/DemoB.java
@@ -0,0 +1,11 @@
+package b;
+
+public class DemoB {
+	public static void main(String[] args) {
+		System.out.println("Tuesday");
+		
+		System.out.println("edris ali edris");
+		System.out.println("yes 23 24 25");
+	}
+
+}
